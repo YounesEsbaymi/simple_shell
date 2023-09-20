33ce3cd12edfde4465c3ced57bdb2848759546cb
YounesEsbaymi <Headlineryounes2023@gmail.com>
2023-09-20 16:36:21 -0700

    0

--- execut.c
+++ execut.c
@@ -2,38 +2,39 @@
 
 int _execute(char **command, char **av, int i)
 {
-    pid_t child_pid;
-    int status;
-    char *cmd;
+	pid_t child_pid;
+	int status;
+	char *cmd;
 
-    if (command[0] == NULL)
-    {
-        free(command);
-        return (0);
-    }
+	if (command[0] == NULL)
+	{
+		free(command);
+		return (0);
+	}
 
-    cmd = _getpath(command[0]);
-    if (!cmd)
-    {
-        printError(av[0], command[0], i);
+	cmd = _getpath(command[0]);
+	if (!cmd)
+	{
+		printError(av[0], command[0], i);
 		_freearr(command);
 		return (127);
-    }
-    child_pid = fork();
+	}
+	child_pid = fork();
 
-    if (child_pid == 0) {
-        if (execvp(cmd, command) == -1)
-        {
-            free(cmd);
-            _freearr(command);
-        }
-    }
-    else
-    {
-        waitpid(child_pid, &status, 0);
-        _freearr(command);
-        free(cmd);
-    }
+	if (child_pid == 0)
+	{
+		if (execvp(cmd, command) == -1)
+		{
+			free(cmd);
+			_freearr(command);
+		}
+	}
+	else
+	{
+		waitpid(child_pid, &status, 0);
+		_freearr(command);
+		free(cmd);
+	}
 
-    return WEXITSTATUS(status);
+	return WEXITSTATUS(satus);
 }
--- getenv.c
+++ getenv.c
@@ -3,88 +3,84 @@
 
 char *_getenv(const char *name)
 {
-    const char *env_str;
-    const char *value;
-    int i = 0;
-    size_t key_len;
+	const char *env_str;
+	const char *value;
+	int i = 0;
+	size_t key_len;
 
-    for (i = 0; environ[i]; i++)
-    {
-        env_str = environ[i];
-        value = _strchr(env_str, '=');
+	for (i = 0; environ[i]; i++)
+	{
+		env_str = environ[i];
+		value = _strchr(env_str, '=');
 
-        if (!value)
-        {
-            continue;
-        }
+		if (!value)
+		{
+			continue;
+		}
 
-        key_len = value - env_str;
-        if (_strncmp(env_str, name, key_len) == 0 && name[key_len] == '\0')
-        {
-            return (char *)(value + 1);
-        }
-    }
+		key_len = value - env_str;
+		if (_strncmp(env_str, name, key_len) == 0 && name[key_len] == '\0')
+		{
+			return ((char *)(value + 1));
+		}
+	}
 
-    return NULL;
+	return (NULL);
 }
 
 char *_getpath(const char *command)
 {
-    const char *path;
-    const char *token;
-    struct stat st;
-    char cmd_path[1024];
-    char *path_copy;
-    int i = 0;
+	const char *path;
+	const char *token;
+	struct stat st;
+	char cmd_path[1024];
+	char *path_copy;
+	int i = 0;
 
-    if (command == NULL)
-    {
-        return NULL;
-    }
+	if (command == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; command[i]; i++)
-    {
-        if (command[i] == '/') {
-            if (stat(command, &st) == 0)
-            {
-                return _strdup(command);
-            }
-            else
-            {
-                return NULL;
-            }
-        }
-    }
+	for (i = 0; command[i]; i++)
+	{
+		if (command[i]== '/')
+		{
+			if (stat(command, &st) == 0)
+			{
+				return (_strdup(command));
+			}
+			else
+			{
+				return (NULL);
+			}
+		}
+	}
 
-    path = _getenv("PATH");
+	path = _getenv("PATH");
 
-    if (!path)
-    {
-        return NULL;
-    }
+	if (!path)
+	{
+		return (NULL);
+	}
 
-    path_copy = _strdup(path);
-    if (!path_copy)
-    {
-        return NULL;
-    }
+	path_copy = _strdup(path);
+	if (!path_copy)
+	{
+		return (NULL);
+	}
+	token = strtok(path_copy, ":");
+	while (token)
+	{
+		_snprintf(cmd_path, sizeof(cmd_path), "%s/%s", token, command);
 
-    token = strtok(path_copy, ":");
-
-    while (token)
-    {
-        _snprintf(cmd_path, sizeof(cmd_path), "%s/%s", token, command);
-
-        if (stat(cmd_path, &st) == 0)
-        {
-            free(path_copy);
-            return _strdup(cmd_path);
-        }
-
-        token = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return NULL;
+		if (stat(cmd_path, &st) == 0)
+		{
+			free(path_copy);
+			return _strdup(cmd_path);
+		}
+		token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return NULL;
 }
-
