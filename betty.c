8744459830129a9d6da9985a18dbd3d58273fe74
YounesEsbaymi <Headlineryounes2023@gmail.com>
2023-09-20 06:30:38 -0700

    user

---
+++ betty.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_message - Just for betty checks.
+ */
+
+void print_message(void)
+{
+	printf("Hello, Betty!\n");
+	printf("I think this code is sufficient for this task\n");
+}
+
+int main(void)
+{
+	print_message();
+	return (0);
+}
