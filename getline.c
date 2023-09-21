a5be8daf06374297253794d5e5b9b8b26af5fe6e
ya7ya99 <yahyakhaldy9@gmail.com>
2023-09-21 09:31:50 -0700

    simple_shell

--- getline.c
+++ getline.c
@@ -19,33 +19,22 @@ size_t _getline(char **line, size_t *line_size, FILE *stream)
         *line_size = 128;
         *line = (char *)malloc(*line_size * sizeof(char));
         if (*line == NULL)
-        {
             return (0);
-        }
     }
-
     while ((c = fgetc(stream)) != EOF && c != '\n')
     {
         (*line)[length] = (char)c;
         length++;
-
         if (length >= *line_size)
         {
             *line_size *= 2;
             *line = (char *)realloc(*line, *line_size * sizeof(char));
             if (*line == NULL)
-            {
                 return (0);
-            }
         }
     }
-
     if (c == EOF && length == 0)
-    {
         return (-1);
-    }
-
     (*line)[length] = '\0';
     return (length);
 }
-
