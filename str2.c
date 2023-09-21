166ea5d9869c68d6224304a635cd6d4ed28637f3
YounesEsbaymi <Headlineryounes2023@gmail.com>
2023-09-20 17:08:51 -0700

    0

--- str2.c
+++ str2.c
@@ -6,11 +6,11 @@ char *_strchr(const char *str, int ch)
 	{
 		if (*str == ch)
 		{
-			return (char *)str; 
+			return ((char *)str);
 		}
 		str++;
 	}
-	return NULL;  
+	return (NULL);
 }
 
 int _strncmp(const char *str1, const char *str2, size_t n)
@@ -35,16 +35,14 @@ int _strncmp(const char *str1, const char *str2, size_t n)
 		}
 	}
 
-	return result;
+	return (result);
 }
 
 int _snprintf(char *str, size_t size, const char *format, ...)
 {
 	int written = 0, remaining;
-
 	va_list args;
 	va_start(args, format);
-
 	remaining = size - 1;
 
 	while (*format != '\0' && remaining > 0)
@@ -60,80 +58,70 @@ int _snprintf(char *str, size_t size, const char *format, ...)
 						int value = va_arg(args, int);
 						char int_str[20];
 						int digits = my_itoa(int_str, value);
+
 						if (digits < 0)
 						{
 							va_end(args);
-							return -1;
+							return (-1);
 						}
-
 						if (digits > remaining)
 						{
 							va_end(args);
-							return -1;
+							return (-1);
 						}
 						_strcpy(str + written, int_str);
 						written += digits;
-                    remaining -= digits;
-                    break;
-                }
-                case 's':
-                {
-                    char *value = va_arg(args, char *);
-                    int length = _strlen(value);
+						remaining -= digits;
+						break;
+					}
+				case 's':
+					{
+						char *value = va_arg(args, char *);
+						int length = _strlen(value);
 
-                    if (length > remaining)
-                    {
-                        va_end(args);
-                        return -1;
-                    }
-                    _strcpy(str + written, value);
-                    written += length;
-                    remaining -= length;
-                    break;
-                }
-
-                default:
-                    va_end(args);
-                    return -1;
-            }
-        }
-        else
-        {
-            str[written] = *format;
-            written++;
-            remaining--;
-        }
-        format++;
-    }
-
-
-    str[written] = '\0';
-
-    va_end(args);
-    return written;
+						if (length > remaining)
+						{
+							va_end(args);
+							return (-1);
+						}
+						_strcpy(str + written, value);
+						written += length;
+						remaining -= length;
+						break;
+					}
+				default:
+					va_end(args);
+					return (-1);
+			}
+		}
+		else
+		{
+			str[written] = *format;
+			written++;
+			remaining--;
+		}
+		format++;
+	}
+	str[written] = '\0';
+	va_end(args);
+	return (written);
 }
-
 int my_itoa(char *str, int num)
 {
-    int digits = 0;
-    int temp = num;
-    int i;
-    
+	int digits = 0;
+	int temp = num;
+	int i;
 
-    while (temp != 0)
-    {
-        temp /= 10;
-        digits++;
-    }
-    
-
-    for (i = digits - 1; i >= 0; i--)
-    {
-        str[i] = '0' + (num % 10);
-        num /= 10;
-    }
-    str[digits] = '\0';
-
-    return digits;
+	while (temp != 0)
+	{
+		temp /= 10;
+		digits++;
+	}
+	for (i = digits - 1; i >= 0; i--)
+	{
+		str[i] = '0' + (num % 10);
+		num /= 10;
+	}
+	str[digits] = '\0';
+	return (digits);
 }
-
